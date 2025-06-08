687575860fb839c5ee668e607e3f333db8342d4e
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-06-08 12:48:55 +0700

    feat : router :: add notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Route>
     )
